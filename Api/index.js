762af591212c6c8febbd6a2529ce5a232829b7bd
Eyub <eyub@eyubs-mbp.home>
2022-05-05 13:56:29 +0100

    added heroku file

--- Api/index.js
+++ Api/index.js
@@ -3,6 +3,6 @@ const http = require('http')
 
 const server = http.createServer(app)
 
-server.listen(3003, () => {
+server.listen(process.env.PORT || 3003, () => {
   console.log('Server up on the port 3003')
 })
